10bc9bf0201f8739e3b1516c45eb3070e0690163
EneDNarf <srlk_doblev@hotmail.com>
2023-02-16 12:06:03 +0100

    using endPoint route from routes file

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 // importamos express en una constante para utilizarlo.
 // const express = require('express'); forma antigua sin BABEL-ENV
 import express from 'express';
+import IndexRoutes from './routes/index';
 
 // our server -> contains all routes, methods, etc.
 const app = express();
+
+app.get('/', (req, res) => {
+  res.json({
+    message : 'Welcome to my COLLECTION-REST-API'
+  })
+});
+
+app.use(IndexRoutes);
 
 // port used
 app.set('port', process.env.PORT || 3030);
