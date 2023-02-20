0a068a950181af80c489c9916e20ce8d020ccd53
EneDNarf <srlk_doblev@hotmail.com>
2023-02-20 01:25:21 +0100

    added try/catch to see connection errors to the db

--- src/database.js
+++ src/database.js
@@ -2,10 +2,15 @@ import mongoose from 'mongoose';
 import config from './config';
 
 (async () => {
-  const db = 
-    await mongoose.connect(config.mongodbURL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
-    console.log('DATABASE CONNECTED TO: ', db.connection.name);
+  try {
+    const db = 
+      await mongoose.connect(config.mongodbURL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      });
+      console.log('DATABASE CONNECTED TO: ', db.connection.name);
+  } catch(err) {
+    console.log('ERROR CONNECTION TO THE DB <<<---------------------<<');
+    console.log(err);
+  }
 })();
