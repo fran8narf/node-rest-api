9b10f65d81a75db61bebf977666434606426413a
EneDNarf <srlk_doblev@hotmail.com>
2023-02-20 01:16:20 +0100

    morgan added to see connections to the API

--- src/app.js
+++ src/app.js
@@ -1,18 +1,17 @@
 // importamos express en una constante para utilizarlo.
 // const express = require('express'); forma antigua sin BABEL-ENV
-// Movemos todo a APP para diferenciar contenidos.
-// APP -> configuración del servidor.
-// index -> arranque del servidor.
-// routes -> definición de las urls.
+
 import express from 'express';
 import GCollectionRoutes from './routes/collection.routes';
 
 // our server -> contains all routes, methods, etc.
 const app = express();
 
+app.set('port', process.env.PORT || 3030);
+
 //JSON
 app.use(express.json());
 
-app.use('/api/collections',GCollectionRoutes);
+app.use('/api/collections', GCollectionRoutes);
 
 export default app;
