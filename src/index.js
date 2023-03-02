a956f14a387b93632cb847a9b97302cc84da13a5
EneDNarf <srlk_doblev@hotmail.com>
2023-03-02 15:23:48 +0100

    added get root message

--- src/index.js
+++ src/index.js
@@ -5,3 +5,7 @@ import './database';
 app.set('port', process.env.PORT || 3030);
 app.listen(app.get('port'));
 console.log(`server on port`, app.get('port'));
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Welcome to my Collection API \(<-<\)' });
+});
