75248a7499ee1aad2529b6eb67a65fd9743d6098
EneDNarf <srlk_doblev@hotmail.com>
2023-03-27 15:34:50 +0200

    añadido el path de la subida

--- src/routes/collection.routes.js
+++ src/routes/collection.routes.js
@@ -3,11 +3,25 @@ import { Router } from 'express';
 import * as collectionCtrl from '../Controllers/collection.controller';
 import * as videoGameCtrl from '../Controllers/videoGame.controller';
 
+import multer from "multer";
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}-${file.originalname}`);
+  },
+});
+
+const upload = multer({ storage });
+
 const router = Router();
 
 // COLLECTIONS
 router.get('/', collectionCtrl.getCollections);
-router.post('/collection', collectionCtrl.addCollection);
+// router.post('/collection', collectionCtrl.addCollection);
+router.post("/collection", upload.single("image"), collectionCtrl.addCollection);
 // router.get('/greater', collectionCtrl.getCollectionsGreaterThan);
 router.get('/collection/:id', collectionCtrl.findOneCollection);
 router.delete('/collection/:id', collectionCtrl.deleteCollection);
